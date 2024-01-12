80fc8881abf18879318572294ac6cce8516835af
vanh <caovietanhhd@gmail.com>
2024-01-12 14:03:47 +0700

    6

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 # DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # ALLWOWED_HOSTS = ['*']
 
 
